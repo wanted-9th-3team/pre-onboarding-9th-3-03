8191e69a0eab8f30acef25678c061434e454c615
rlawodnr1109 <wodnr1109@naver.com>
2023-03-15 10:41:05 +0900

    feat: filter 기능 구현

--- src/Types.ts
+++ src/Types.ts
@@ -16,8 +16,16 @@ export interface IDataBase {
   value_area: number
   value_bar: number
 }
+
 export interface IDataType {
   timeData: string[]
   areaData: number[]
   barData: number[]
+}
+
+export interface IFiterState {
+  성북구: boolean
+  강남구: boolean
+  노원구: boolean
+  중랑구: boolean
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ChartView from './ChartView'
 import getChartInfo from '../apis/chartApi'
 import mock_data from '../data/mock_data.json'
-import { IDataBase } from '../Types'
+import { IDataBase, IFiterState } from '../Types'
 
 function Home() {
   const chartData = mock_data.response
@@ -35,7 +35,77 @@ function Home() {
     return data.time
   })
 
-  return <ChartView timeData={timeData} areaData={areaData} barData={barData} />
+  const [filterState, setFilterState] = React.useState<IFiterState>({
+    성북구: true,
+    강남구: true,
+    노원구: true,
+    중랑구: true,
+  })
+
+  const filterHandler = () => {
+    setDataBaseState(
+      DataBase.filter(data => {
+        return (
+          (filterState.성북구 && data.id === '성북구') ||
+          (filterState.강남구 && data.id === '강남구') ||
+          (filterState.노원구 && data.id === '노원구') ||
+          (filterState.중랑구 && data.id === '중랑구')
+        )
+      })
+    )
+  }
+
+  const checkoboxHandler = e => {
+    const { name, defaultChecked } = e.target
+    setFilterState({
+      ...filterState,
+      [name]: !defaultChecked,
+    })
+  }
+
+  return (
+    <>
+      <ChartView timeData={timeData} areaData={areaData} barData={barData} />
+
+      <div>
+        <input
+          type="checkbox"
+          name="성북구"
+          defaultChecked={filterState.성북구}
+          onClick={checkoboxHandler}
+        />
+        <label>성북구</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          name="강남구"
+          defaultChecked={filterState.강남구}
+          onClick={checkoboxHandler}
+        />
+        <label>강남구</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          name="노원구"
+          defaultChecked={filterState.노원구}
+          onClick={checkoboxHandler}
+        />
+        <label>노원구</label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          name="중랑구"
+          defaultChecked={filterState.중랑구}
+          onClick={checkoboxHandler}
+        />
+        <label>중랑구</label>
+      </div>
+      <button onClick={filterHandler}>확인</button>
+    </>
+  )
 }
 
 export default Home
