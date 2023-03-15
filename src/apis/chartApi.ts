4b9774a08c089efba3f06abae16d29e133f4547e
bona <bona373737@gmail.com>
2023-03-15 12:44:48 +0900

    apexchart패키지설치, 차트데이터 api, chart렌더링

--- src/apis/chartApi.ts
+++ src/apis/chartApi.ts
@@ -1,13 +1,41 @@
 import axios from 'axios'
 
 const getChartInfo = async () => {
-  const response = await axios('mock_data.json')
+  let response
+  const xAxisData: string[] = []
+  const areaYAxisData: number[] = []
+  const barYAxisData: number[] = []
+  const filterData: string[] = []
 
-  if (response.status === 200) {
-    const { data } = response
-    return data
+  try {
+    response = await axios('mock_data.json')
+    response = response.data.response
+  } catch (error) {
+    throw new Error(error)
   }
-  return null
+
+  for (let i = 0; i < Object.keys(response).length; i++) {
+    const key: string = Object.keys(response)[i]
+    const areaValue: number = Object.values(response)[i].value_area
+    const barValue: number = Object.values(response)[i].value_bar
+    const idValue: string = Object.values(response)[i].id
+
+    xAxisData.push(key)
+    areaYAxisData.push(areaValue)
+    barYAxisData.push(barValue)
+    filterData.push(idValue)
+
+    // if (!filterData.includes(idValue)) {
+    //   filterData.push(idValue)
+    // }
+  }
+
+  return {
+    xAxisData,
+    areaYAxisData,
+    barYAxisData,
+    filterData,
+  }
 }
 
 export default getChartInfo
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,168 @@
-import styles from './Home.module.css'
-
+import React, { Component, useEffect, useState } from 'react'
+import Chart from 'react-apexcharts'
+import getChartInfo from '../apis/chartApi'
+import filter from '../utils/filter'
+// import styles from './Home.module.css'
 function Home() {
-  return <h1 className={styles.homeHeader}>Hello World</h1>
+  const [xAxisData, setXAxisData] = useState<string[]>()
+  const [filterData, setFilterData] = useState<string[]>()
+  const [areaYAxisData, setAreaYAxisData] = useState<number[]>()
+  const [barYAxisData, setBarYAxisData] = useState<number[]>()
+
+  useEffect(() => {
+    getChartInfo().then(data => {
+      setXAxisData(data.xAxisData)
+      setAreaYAxisData(data.areaYAxisData)
+      setBarYAxisData(data.barYAxisData)
+      setFilterData(data.filterData)
+    })
+  }, [])
+
+  const state = {
+    series: [
+      {
+        name: 'Area',
+        type: 'area',
+        data: areaYAxisData,
+      },
+      {
+        name: 'Bar',
+        type: 'column',
+        data: barYAxisData,
+      },
+      {
+        name: 'id',
+        data: filterData,
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: 'line',
+        stacked: false,
+      },
+      stroke: {
+        width: [0, 2, 5],
+        curve: 'smooth',
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '50%',
+        },
+      },
+      // fill: {
+      //   colors: [
+      //     function ({ value, seriesIndex, w }) {
+      //       console.log(w.config)
+      //       if (value < 55) {
+      //         return '#7E36AF'
+      //       }
+      //       if (value >= 55 && value < 80) {
+      //         return '#164666'
+      //       }
+      //       return '#D9534F'
+      //     },
+      //   ],
+      // },
+      labels: xAxisData,
+      markers: {
+        size: 0,
+      },
+      xaxis: {
+        type: 'datetime',
+      },
+      yaxis: [
+        {
+          seriesName: 'Area',
+          opposite: true,
+          min: 0,
+          max: 300,
+          axisTicks: {
+            show: true,
+          },
+          axisBorder: {
+            show: true,
+            color: '#e30000',
+          },
+          labels: {
+            style: {
+              colors: '#e30000',
+            },
+          },
+          title: {
+            text: 'Area',
+            style: {
+              color: '#e30000',
+            },
+          },
+        },
+        {
+          seriesName: 'Bar',
+          min: 0,
+          max: 20000,
+          axisTicks: {
+            show: true,
+          },
+          axisBorder: {
+            show: true,
+            color: '#00E396',
+          },
+          labels: {
+            style: {
+              colors: '#00E396',
+            },
+          },
+          dataLabels: false,
+          title: {
+            text: 'Bar',
+            style: {
+              color: '#00E396',
+            },
+          },
+        },
+      ],
+      tooltip: {
+        shared: true,
+        intersect: false,
+        y: {
+          formatter(y) {
+            return y
+          },
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      chart: {
+        toolbar: {
+          show: false,
+        },
+      },
+    },
+  }
+
+  return (
+    <div className="app">
+      {/* <section className="btn_wrap">
+        {filterData &&
+          filterData.map((item, index) => {
+            return <button key={index}>{item}</button>
+          })}
+      </section> */}
+
+      <div className="row">
+        <div className="mixed-chart">
+          <Chart
+            options={state.options}
+            series={state.series}
+            type="bar"
+            width="1200"
+            height={500}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
