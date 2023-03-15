a05d79e53fae661ff67986fba56a053aadd2e14e
michoball <myunghun0114@gmail.com>
2023-03-16 02:32:56 +0900

    feat: Make Chart Types

--- src/Types.ts
+++ src/Types.ts
@@ -1,11 +1,23 @@
-// 타입 지정 파일 - 수정해서 시작
-export interface ITripInfo {
-  idx: number
-  name: string
-  mainImage: string
-  description: string
-  spaceCategory: string
-  price: number
-  maximumPurchases: number
-  registrationDate: string
+import { ChartDataset, ChartTypeRegistry } from 'chart.js'
+
+export interface ITableInfo {
+  id: string
+  value_area: number
+  value_bar: number
 }
+
+export interface ITableList {
+  [key: string]: ITableInfo
+}
+
+export type TCustomChartData = { id: string; x: string; y: number }
+
+export type TChartDataset = ChartDataset<
+  keyof ChartTypeRegistry,
+  TCustomChartData[]
+>
+
+// export interface IMyChartData {
+//   labels: string[]
+//   datasets: TChartDataset[]
+// }
