213b7c76f985ddb69e22d249778e89e220a9a877
skyhanull <anky0425@naver.com>
2023-03-16 16:41:28 +0900

    feat: app컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Home from './components/Home'
-
 import './App.css'
+import getChartInfo from './apis/chartApi'
+import { IChartList } from './Types'
 
 function App() {
-  return <Home />
+  const [apidata, setpidata] = useState<IChartList>({})
+
+  useEffect(() => {
+    const apiHandler = async () => {
+      const res = await getChartInfo()
+
+      if (res) {
+        setpidata(res.response)
+      }
+    }
+    apiHandler()
+  }, [])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/:id" element={<Chart apidata={apidata} />} /> */}
+        <Route path="/:id" element={<Home data={apidata} />} />
+        <Route path="/" element={<Home data={apidata} />} />
+      </Routes>
+
+      {/* <Home apidata={apidata} /> */}
+    </BrowserRouter>
+  )
 }
 
 export default App
