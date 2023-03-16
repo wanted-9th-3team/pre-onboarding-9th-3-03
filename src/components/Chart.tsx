e193fa0b450ff6d00a8b6d5858dcb811b2965a1a
skyhanull <anky0425@naver.com>
2023-03-16 15:42:33 +0900

    refcator: 주석 정리및 변수명 정리

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -10,76 +10,68 @@ interface Ifilter {
   value_area: number
   value_bar: number
 }
-function Chart({ apidata, GGK }: any) {
+function Chart({ apidata }: any) {
   const { id } = useParams()
 
-  const Kryof: any[] = apidata ? Object.entries(apidata) : []
-  // console.log(Kryof)
-  const AS =
+  const DataEntries: any[] = apidata ? Object.entries(apidata) : []
+
+  const filterId =
     id !== undefined
-      ? Kryof.filter(n => n.find((k: any) => k.id === id))
-      : Kryof
+      ? DataEntries.filter(n => n.find((k: any) => k.id === id))
+      : DataEntries
   // console.log(AS.map(el => el[0]))
-  const PPP: string[] = AS.map(el => el[0])
+
   // console.log(PPP)
 
   const KKK: Ifilter[] = apidata ? Object.values(apidata) : []
-  console.log(KKK)
-  const FD = Object.keys(apidata).map(el => el)
-  // console.log(FD)
-  const filtered =
-    id !== undefined
-      ? Object.values(KKK).filter(names => names.id === id)
-      : Object.values(KKK)
-  // console.log(filtered)
 
-  const colorHandler = () => {
-    if (!Object.values(KKK).filter(names => names.id === id)) {
-      return ['Red', 'orange']
-    }
-    return ['green', 'blue']
-  }
-  const filterdata = Object.values(KKK).filter(names => names.id === id)
-  console.log(filterdata)
+  // const filtered =
+  //   id !== undefined
+  //     ? Object.values(KKK).filter(names => names.id === id)
+  //     : Object.values(KKK)
 
-  const fififi = Object.values(KKK).map(el => el.value_area)
-  console.log([...fififi, 110])
+  // const colorHandler = () => {
+  //   if (!Object.values(KKK).filter(names => names.id === id)) {
+  //     return ['Red', 'orange']
+  //   }
+  //   return ['green', 'blue']
+  // }
 
-  const datalist = [
-    {
-      name: '내일의 기온',
-      type: 'area',
-      data: fififi,
-      color: Object.values(KKK).map(names =>
-        names.id === id ? '#FF0000' : '#02DFDE'
-      ),
-    },
-    {
-      name: '내일의 기온',
-      type: 'bar',
-      data: Object.values(KKK).map(el => el.value_bar),
-      color: '#41B883',
-    },
-  ]
+  // const datalist = [
+  //   {
+  //     name: '내일의 기온',
+  //     type: 'area',
+  //     data: fififi,
+  //     color: Object.values(KKK).map(names =>
+  //       names.id === id ? '#FF0000' : '#02DFDE'
+  //     ),
+  //   },
+  //   {
+  //     name: '내일의 기온',
+  //     type: 'bar',
+  //     data: Object.values(KKK).map(el => el.value_bar),
+  //     color: '#41B883',
+  //   },
+  // ]
 
   const color = Object.values(KKK).filter(names => names.id === id)
     ? ['blue', 'pink']
     : ['#d4526e', '#d4526e']
 
-  const KKKF = function () {
-    Object.values(KKK).map(names => (names.id === id ? '#FF0000' : '#02DFDE'))
-  }
+  // const KKKF = function () {
+  //   Object.values(KKK).map(names => (names.id === id ? '#FF0000' : '#02DFDE'))
+  // }
   return (
     <div>
       <ApexChart
         series={[
-          // {
-          //   name: '내일의 기온',
-          //   type: 'area',
-          //   data: Object.values(filterdata).map(el => el.value_bar),
+          {
+            name: '내일의 기온',
+            type: 'area',
+            data: Object.values(KKK).map(el => el.value_bar),
 
-          //   color: id ? 'pink' : 'red',
-          // },
+            color: id ? 'pink' : 'red',
+          },
           {
             name: '내일의 기온',
             type: 'bar',
